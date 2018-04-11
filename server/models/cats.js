041ef2452c28d29070dfe2e75dd8c0292ac73187
dmshikoff <dmshikoff@gmail.com>
2018-04-11 10:35:23 -0700

    added janky .json database

--- server/models/cats.js
+++ server/models/cats.js
@@ -1,13 +1,19 @@
+const fs = require('fs')
+const path = require('path')
 const shortid = require('shortid')
-
-// data store
-let cats = []
+const file = path.join(__dirname, 'db.json')
 
 function getAll(){
+  const contents = fs.readFileSync(file, 'utf-8')
+  const cats = JSON.parse(contents)
+  
   return { data: cats }
 }
 
 function getOne(id){
+  const contents = fs.readFileSync(file, 'utf-8')
+  const cats = JSON.parse(contents)
+
   const cat = cats.find(cat => cat.id === id)
 
   if(cat) {
@@ -19,17 +25,24 @@ function getOne(id){
 }
 
 function create(name){
+  const contents = fs.readFileSync(file, 'utf-8')
+  const cats = JSON.parse(contents)
+
   const cat = { id: shortid.generate(), name }
   cats.push(cat)
+  fs.writeFileSync(file, JSON.stringify(cats))
 
   return { data: cat }
 }
 
 function update(id, name){
+  const contents = fs.readFileSync(file, 'utf-8')
+  let cats = JSON.parse(contents)
   const cat = cats.find(cat => cat.id === id)
 
   if(cat){
     cat.name = name
+    fs.writeFileSync(file, JSON.stringify(cats))
     return { data: cat}
   }
   else {
@@ -38,11 +51,14 @@ function update(id, name){
 }
 
 function remove(id){
+  const contents = fs.readFileSync(file, 'utf-8')
+  let cats = JSON.parse(contents)
   const cat = cats.find(cat => cat.id === id)
   
   if(cat){
     cats = cats.filter(cat => cat.id !== id)
     delete cat.id
+    fs.writeFileSync(file, JSON.stringify(cats))
     return { data: cat}
   }
   else {
